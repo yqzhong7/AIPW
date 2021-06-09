d71826c8fba7c9b3235c9ca2affc3ee1ecf74625
YZ <yq.zhong7@gmail.com>
2021-06-08 22:43:11 -0400

    revert to commit 6bd14feb7699de251197425e858e34b5453a16c3

--- tests/testthat/test-fit.R
+++ tests/testthat/test-fit.R
@@ -50,6 +50,54 @@ test_that("AIPW fit: SuperLeaner & k_split", {
   expect_true(is.null(aipw$estimate))
 })
 
+#' @title Testing fit: sl3 & k_split
+#' @section Last Updated By:
+#' Yongqi Zhong
+#' @section Last Update Date:
+#' 2020/08/09
+test_that("AIPW fit: sl3 & k_split", {
+  require(sl3)
+  ##k_split == 1: no cross-fitting
+  vec <- function() sample(0:1,100,replace = T)
+  lrnr_glm <- sl3::Lrnr_glm$new()
+  lrnr_mean <- sl3::Lrnr_mean$new()
+  stacklearner <- sl3::Stack$new(lrnr_glm, lrnr_mean)
+  metalearner <- sl3::Lrnr_nnls$new()
+  sl3.lib <- sl3::Lrnr_sl$new(learners = stacklearner,
+                              metalearner = metalearner)
+  aipw <-  AIPW$new(Y=vec(),
+                    A=vec(),
+                    W.Q =vec(),
+                    W.g =vec(),
+                    Q.SL.library=sl3.lib,
+                    g.SL.library=sl3.lib,
+                    k_split = 1,verbose = FALSE,
+                    save.sl.fit = TRUE)
+  aipw$fit()
+  expect_false(any(sapply(aipw$libs, is.null)))
+  expect_false(any(sapply(aipw$obs_est[1:4], is.na))) #mu - raw_p_score
+  expect_true(any(sapply(aipw$obs_est[5:7], is.null)))
+  expect_true(is.null(aipw$result))
+  expect_true(is.null(aipw$estimate))
+
+  ##k_split >=3: cross-fitting == k_split
+  aipw <-  AIPW$new(Y=vec(),
+                    A=vec(),
+                    W.Q =vec(),
+                    W.g =vec(),
+                    Q.SL.library=sl3.lib,
+                    g.SL.library=sl3.lib,
+                    k_split = 3,verbose = FALSE,
+                    save.sl.fit = TRUE)
+  aipw$fit()
+  expect_false(any(sapply(aipw$libs, is.null)))
+  expect_false(any(sapply(aipw$obs_est[1:4], is.na))) #mu - raw_p_score
+  expect_true(any(sapply(aipw$obs_est[5:7], is.null)))
+  expect_true(is.null(aipw$result))
+  expect_true(is.null(aipw$estimate))
+})
+
+
 #' @title Testing fit: verbose and progressr
 #' @section Last Updated By:
 #' Yongqi Zhong
