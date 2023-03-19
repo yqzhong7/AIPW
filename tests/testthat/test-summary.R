5624f107834b1f58f4bef0c05b991cbd0477f4e2
Yongqi Zhong <32525464+yqzhong7@users.noreply.github.com>
2023-03-19 15:06:10 -0400

    Repeated cross-fitting to reduce randomness (#38)
    * remove sl3 and tmle3 supports
    
    * remove sl3 in unit tests
    
    * fix bug and continue removing sl3 tests
    
    * remove sl3 again!
    
    * remove check on R 3.6 for GH action
    
    * added documentation for Repeated class
    
    * update pkgdown yaml for ghactions
    
    * remove pkgdown pages when pr

--- tests/testthat/test-summary.R
+++ tests/testthat/test-summary.R
@@ -42,58 +42,6 @@ test_that("AIPW summary: SuperLeaner & k_split", {
   expect_false(is.null(aipw$result))
 })
 
-#' @title Testing summary: sl3 & k_split
-#' @section Last Updated By:
-#' Yongqi Zhong
-#' @section Last Update Date:
-#' 2021/01/20
-test_that("AIPW summary: sl3 & k_split", {
-  require(sl3)
-  ##k_split == 1: no cross-fitting
-  vec <- function() sample(0:1,100,replace = T)
-  lrnr_glm <- sl3::Lrnr_glm$new()
-  lrnr_mean <- sl3::Lrnr_mean$new()
-  stacklearner <- sl3::Stack$new(lrnr_glm, lrnr_mean)
-  metalearner <- sl3::Lrnr_nnls$new()
-  sl3.lib <- sl3::Lrnr_sl$new(learners = stacklearner,
-                              metalearner = metalearner)
-  aipw <-  AIPW$new(Y=vec(),
-                    A=vec(),
-                    W.Q =vec(),
-                    W.g =vec(),
-                    Q.SL.library=sl3.lib,
-                    g.SL.library=sl3.lib,
-                    k_split = 1,verbose = TRUE,
-                    save.sl.fit = TRUE)
-  #correctly print output
-  expect_output(aipw$fit()$summary(), regexp = "Estimate")
-  #check any null values after calculating results
-  expect_false(any(sapply(aipw$estimates, is.null)))
-  expect_false(any(sapply(aipw$libs, is.null)))
-  expect_false(any(sapply(aipw$obs_est, is.null)))
-  expect_false(is.null(aipw$result))
-  #check results with RR and OR
-  expect_equal(nrow(aipw$result),5)
-
-  ##k_split >0: cross-fitting == k_split
-  aipw <-  AIPW$new(Y=vec(),
-                    A=vec(),
-                    W.Q =vec(),
-                    W.g =vec(),
-                    Q.SL.library=sl3.lib,
-                    g.SL.library=sl3.lib,
-                    k_split = 3,verbose = TRUE,
-                    save.sl.fit = TRUE)
-  #correctly print output
-  expect_output(aipw$fit()$summary(), regexp = "Estimate")
-  #check any null values after calculating results
-  expect_false(any(sapply(aipw$estimates, is.null)))
-  expect_false(any(sapply(aipw$libs, is.null)))
-  expect_false(any(sapply(aipw$obs_est, is.null)))
-  expect_false(is.null(aipw$result))
-  #check results with RR and OR
-  expect_equal(nrow(aipw$result),5)
-})
 
 
 #' @title Testing summary: verbose
